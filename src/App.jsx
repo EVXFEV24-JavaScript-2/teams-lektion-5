c7b272f1986d306befe2a994997d2c08a8a6bfcc
EnanderW <williamenander@gmail.com>
2024-11-04 15:50:56 +0100

    finish examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-import './App.css'
+import "./App.css";
 
 // HTTP, API:er
 // fetch
 // Promises, .then, .catch
 // response objektet
 // Trådar och async/await
-// Varför await på fetch och .json? 
+// Varför await på fetch och .json?
 
-function App() {
-  return (
-    <>
-    </>
-  )
+async function fetchProducts() {
+  const res = await fetch("https://dummyjson.com/products");
+  if (res !== 200) {
+    alert("Failure, try again.");
+    return null;
+  }
+
+  const json = await res.json();
+  return json;
 }
 
-export default App
+function App() {
+  // callback hell
+  // syntax sugar
+  fetchProducts().then((res) => {
+    console.log(res);
+  });
+
+  fetch("https://dummyjson.com/products")
+    .then((res) => {
+      console.log(res);
+      if (res.status != 200) {
+        alert("Attempt failed, please try again.");
+      }
+
+      return res.json();
+    })
+    .then(console.log)
+    .catch((err) => {});
+
+  return (
+    <>
+      <button>Click Me!</button>
+    </>
+  );
+}
+
+export default App;
